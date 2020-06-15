80613f0ffe43b6ca75bbec6eb6f14e2c057ee61f
TiffanyP <tiffanytnp7@gmail.com>
2020-06-15 15:58:07 +0800

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -20,9 +20,8 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL:
-				'https://warm-retreat-78298.herokuapp.com/auth/google/callback'
-			//proxy: true
+			callbackURL: '/auth/google/callback',
+			proxy: true
 		},
 		(accessToken, refreshToken, profile, done) => {
 			User.findOne({ googleId: profile.id }).then((existingUser) => {
