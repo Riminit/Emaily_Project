f2c9ca66a6b7e3dff195c31271bea69b9dd36824
TiffanyP <tiffanytnp7@gmail.com>
2020-06-15 18:46:26 +0800

    debugging

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,5 @@ module.exports = {
 	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
 	mongoURI: process.env.MONGO_URI,
-	cookieKey: process.env.COOKIE_KEY,
-	absoluteURI: 'http://warm-retreat-78298.herokuapp.com'
+	cookieKey: process.env.COOKIE_KEY
 };
