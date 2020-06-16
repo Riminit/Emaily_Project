17d333ba4f816b9ab2c456df4e8375dfc9349c16
TiffanyP <tiffanytnp7@gmail.com>
2020-06-16 15:16:24 +0800

    debugging

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,23 +1,27 @@
 const passport = require('passport');
 
-module.exports = (app) => {
-	// authenticate
-	app.get(
-		'/auth/google',
-		passport.authenticate('google', {
-			scope: ['profile', 'email']
-		})
-	);
+try {
+	module.exports = (app) => {
+		// authenticate
+		app.get(
+			'/auth/google',
+			passport.authenticate('google', {
+				scope: ['profile', 'email']
+			})
+		);
 
-	// redirect back to server
-	app.get('/auth/google/callback', passport.authenticate('google'));
+		// redirect back to server
+		app.get('/auth/google/callback', passport.authenticate('google'));
 
-	app.get('/api/logout', (req, res) => {
-		req.logout();
-		res.send(req.user);
-	});
+		app.get('/api/logout', (req, res) => {
+			req.logout();
+			res.send(req.user);
+		});
 
-	app.get('/api/current_user', (req, res) => {
-		res.send(req.user);
-	});
-};
+		app.get('/api/current_user', (req, res) => {
+			res.send(req.user);
+		});
+	};
+} catch (err) {
+	next(err);
+}
