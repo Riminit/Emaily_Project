d56356f416010eacf893b33b98676c100152f40f
TiffanyP <tiffanytnp7@gmail.com>
2020-06-16 18:24:40 +0800

    debugging

--- services/passport.js
+++ services/passport.js
@@ -27,22 +27,21 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 			proxy: true
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id })
-				.then((existingUser) => {
-					if (existingUser) {
-						// we already have a record with the given profile ID
-						done(null, existingUser);
-					} else {
-						//we don't have a user record with this ID, make a new record
-						new User({ googleId: profile.id })
-							.save()
-							.then((user) => done(null, user));
-					}
-				})
-				.catch((err) => {
-					console.log(err);
-				});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			//.then((existingUser) => {
+			if (existingUser) {
+				// we already have a record with the given profile ID
+				return done(null, existingUser);
+			} else {
+				//we don't have a user record with this ID, make a new record
+				const user = new User({ googleId: profile.id }).save();
+				return done(null, user);
+			}
+			//})
+			// .catch((err) => {
+			// 	console.log(err);
+			// });
 		}
 	)
 );
