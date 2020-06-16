3423be53e4f503bc3a7f8107a5eb8d2505c669d7
TiffanyP <tiffanytnp7@gmail.com>
2020-06-16 15:05:49 +0800

    debugging

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,13 +11,16 @@ module.exports = (app) => {
 
 	// redirect back to server
 	app.get('/auth/google/callback', passport.authenticate('google'));
+	try {
+		app.get('/api/logout', (req, res) => {
+			req.logout();
+			res.send(req.user);
+		});
 
-	app.get('/api/logout', (req, res) => {
-		req.logout();
-		res.send(req.user);
-	});
-
-	app.get('/api/current_user', (req, res) => {
-		res.send(req.user);
-	});
+		app.get('/api/current_user', (req, res) => {
+			res.send(req.user);
+		});
+	} catch (err) {
+		console.log(err);
+	}
 };
