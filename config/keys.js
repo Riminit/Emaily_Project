9b9f65d4ab971e5a958fddaaf7c927fc3f6dfbee
TiffanyP <tiffanytnp7@gmail.com>
2020-06-16 20:36:24 +0800

    debugging

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 if (process.env.NODE_ENV === 'production') {
-	console.log('USED PROD.js!!!!');
 	// we are in prdouction - return the prod set of keys
 	module.exports = require('./prod');
 } else {
