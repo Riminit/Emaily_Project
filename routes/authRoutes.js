567abffca789f6f6d2693ea22ba68f2bc9413b27
TiffanyP <tiffanytnp7@gmail.com>
2020-06-16 15:14:44 +0800

    debugging

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,16 +11,13 @@ module.exports = (app) => {
 
 	// redirect back to server
 	app.get('/auth/google/callback', passport.authenticate('google'));
-	try {
-		app.get('/api/logout', (req, res) => {
-			req.logout();
-			res.send(req.user);
-		});
 
-		app.get('/api/current_user', (req, res) => {
-			res.send(req.user);
-		});
-	} catch (err) {
-		console.log(err);
-	}
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	});
 };
--- services/passport.js
+++ services/passport.js
@@ -4,44 +4,40 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
+try {
+	passport.serializeUser((user, done) => {
+		done(null, user.id);
+	});
 
-passport.serializeUser((user, done) => {
-	done(null, user.id);
-});
+	passport.deserializeUser((id, done) => {
+		User.findById(id).then((user) => {
+			done(null, user);
+		});
+	});
 
-passport.deserializeUser((id, done) => {
-	User.findById(id)
-		.then((user) => {
-			return done(null, user);
-		})
-		.catch((err) => alert(err));
-});
-
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback',
-			proxy: true
-		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id })
-				.then((existingUser) => {
+	passport.use(
+		new GoogleStrategy(
+			{
+				clientID: keys.googleClientID,
+				clientSecret: keys.googleClientSecret,
+				callbackURL: '/auth/google/callback',
+				proxy: true
+			},
+			(accessToken, refreshToken, profile, done) => {
+				User.findOne({ googleId: profile.id }).then((existingUser) => {
 					if (existingUser) {
 						// we already have a record with the given profile ID
-						return done(null, existingUser);
+						done(null, existingUser);
 					} else {
 						//we don't have a user record with this ID, make a new record
 						new User({ googleId: profile.id })
 							.save()
-							.then((user) => {
-								return done(null, user);
-							})
-							.catch((err) => alert(err));
+							.then((user) => done(null, user));
 					}
-				})
-				.catch((err) => alert(err));
-		}
-	)
-);
+				});
+			}
+		)
+	);
+} catch (err) {
+	next(err);
+}
